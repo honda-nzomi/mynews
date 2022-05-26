c8176baa113efbe81386a23a5acc72f357e10329
s15_honnda_nozomi <noa1125ho@gmail.com>
2022-05-26 11:47:56 +0000

    laravel11の課題

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,14 +1,17 @@
-@extends('layouts.profile')
-{{-- purofile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
-@section('title', 'プロフィールの新規作成')
+
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'ニュースの新規作成')
+
 {{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール新規作成</h2>
+                <h2>ニュース新規作成</h2>
             </div>
         </div>
     </div>
 @endsection
- 
