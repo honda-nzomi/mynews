9d5be91a0ece405baa314536bfe42d68c7b7d640
s15_honnda_nozomi <noa1125ho@gmail.com>
2022-06-01 08:12:36 +0000

    laravel14課題提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,23 +4,40 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Profile;
 
 class ProfileController extends Controller
 {
-    public function add()
-    {
-        return view('admin.profile.create');
-    }
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    } 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+  public function add()
+  {
+    return view('admin.profile.create');
+  }
+   
+    
+    // 以下を追記
+  public function create(Request $request)
+  {
+    // 以下を追記
+    // Varidationを行う
+    $this->validate($request, Profile::$rules);
+    $profile = new Profile;
+    $form = $request->all();
+   
+    // フォームから送信されてきた_tokenを削除する
+    unset($form['_token']);
+    
+    // データベースに保存する
+    $profile->fill($form);
+    $profile->save();
+    // admin/news/createにリダイレクトする
+    return redirect('admin/profile/create');
+  }  
+  public function edit()
+  {
+      return view('admin.profile.edit');
+  } 
+  public function update()
+  {
+      return redirect('admin/profile/edit');
+  }
 }
